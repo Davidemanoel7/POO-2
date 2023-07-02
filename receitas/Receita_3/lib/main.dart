1892e033783d669fa3a4b77d6c8bb00afa3cad56
david <david.emanoel.706@ufrn.edu.br>
2023-07-02 11:42:03 -0300

    Receitas POO2

---
+++ receitas/Receita_3/lib/main.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  MyApp app = MyApp();
+  runApp(app);
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(primarySwatch: Colors.deepPurple),
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: MyAppBar(),
+          body: DataBodyWidget(objects: const [
+            "La Fin Du Monde - Bock - 65 ibu",
+            "Sapporo Premiume - Sour Ale - 54 ibu",
+            "Duvel - Pilsner - 82 ibu"
+          ]),
+          bottomNavigationBar: NewNavBar(icons: const [
+            Icons.ac_unit,
+            Icons.access_alarm,
+            Icons.accessibility
+          ]),
+        ));
+  }
+}
+
+class NewNavBar extends StatelessWidget {
+  final List icons;
+  NewNavBar({required this.icons});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+        items: icons
+            .map((data) =>
+                BottomNavigationBarItem(icon: Icon(data), label: "Teste"))
+            .toList());
+  }
+}
+
+class DataBodyWidget extends StatelessWidget {
+  List<String> objects;
+
+  DataBodyWidget({this.objects = const []});
+
+  Expanded processarUmElemento(String obj) {
+    return Expanded(
+      child: Center(child: Text(obj)),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        children: objects
+            .map((obj) => Expanded(
+                  child: Center(child: Text(obj)),
+                ))
+            .toList());
+  }
+}
+
+class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  Colors? selected;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: const Text("Dicas"),
+      actions: [
+        PopupMenuButton(
+            onSelected: (value) {
+              // TODO: Later add the color change functionality
+            },
+            itemBuilder: (context) => const [
+                  PopupMenuItem(
+                    value: Colors.black,
+                    child: Text("Black"),
+                  ),
+                  PopupMenuItem(
+                    value: Colors.amber,
+                    child: Text("Amber"),
+                  ),
+                  PopupMenuItem(
+                    value: Colors.cyan,
+                    child: Text("Cyan"),
+                  ),
+                ])
+      ],
+    );
+  }
+}
